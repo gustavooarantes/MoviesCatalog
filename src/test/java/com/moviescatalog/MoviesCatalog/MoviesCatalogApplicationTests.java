98edaa9a854853698af62d08c3429fca086b8b76
gustavooarantes <profissional.gustavoa@gmail.com>
2025-06-28 16:22:05 -0300

    Initial maven project

---
+++ src/test/java/com/moviescatalog/MoviesCatalog/MoviesCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviescatalog.MoviesCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
